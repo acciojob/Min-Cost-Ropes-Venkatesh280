25d7d4a3eb952f1e3adf3b995c91caa3c5991790
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2025-01-28 19:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function mincost(arr)
 { 
+	function mincost(arr) {
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return arr[0];
+
+  // Create a MinHeap
+  const heap = new MinHeap((a, b) => a - b);
+  arr.forEach((num) => heap.push(num));
+
+  let totalCost = 0;
+
+  // Merge elements until only one remains
+  while (heap.size() > 1) {
+    const first = heap.pop();
+    const second = heap.pop();
+    const mergeCost = first + second;
+    totalCost += mergeCost;
+    heap.push(mergeCost);
+  }
+
+  return totalCost;
 //write your code here
 // return the min cost
   
