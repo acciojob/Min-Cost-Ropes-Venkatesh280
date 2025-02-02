f980bb8cdde1f41ae818d3e2b77a349153f2a4f7
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2025-02-02 09:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function mincost(arr)
 { 
-	function mincost(arr) {
-  if (arr.length === 0) return 0;
+	if (arr.length === 0) return 0;
   if (arr.length === 1) return arr[0];
 
   // Create a MinHeap
